114ea1d83bb7869e4758cf88f8e95dc5d4708611
Rafael Silva <rafharls33@gmail.com>
2026-08-26 17:03:56 -0300

    refactor(gateway): simplify error handling

--- backend/gateway/src/middlewares/errorHandler.js
+++ backend/gateway/src/middlewares/errorHandler.js
@@ -1,14 +1,8 @@
-const AppError = require('../../errors/AppError');
 const logger = require('../config/logger');
 
 const errorHandler = (error, req, res, next) => {
-  if (error instanceof AppError) {
-    return res.status(error.statusCode).json({
-      message: error.message,
-    });
-  }
-
   logger.error(error.message, { stack: error.stack });
+
   return res.status(500).json({
     message: 'Erro interno no servidor.',
   });
