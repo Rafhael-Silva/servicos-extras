faa446c6aa342ca09911149dba8ce4642176dc5c
Rafael Silva <rafharls33@gmail.com>
2026-09-15 23:12:12 -0300

    feat(front): configure toast notifications

--- frontend/src/app/providers/AppProviders.jsx
+++ frontend/src/app/providers/AppProviders.jsx
@@ -1,7 +1,13 @@
 import SessionProvider from './SessionProvider';
+import { Toaster } from 'react-hot-toast';
 
 function AppProviders({ children }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <>
+      <SessionProvider>{children}</SessionProvider>
+      <Toaster position="top-right" />
+    </>
+  );
 }
 
 export default AppProviders;
